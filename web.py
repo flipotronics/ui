093b0a5b74ba269838525385040ab4beefcf0e8a
mathias-dietrich <mathias.dietrich@gmail.com>
2019-06-04 13:46:36 -0700

    init

---
+++ web.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    body = "<h1 style='color:blue'>Environment variables</h1>"
+    for k, v in os.environ.items(): 
+    	body += k + "=" + v + "<br/>"
+
+    return body
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
