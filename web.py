1ec0167b7237241f45f83fe5605fd2acd1014fcb
mathias-dietrich <mathias.dietrich@gmail.com>
2019-06-04 17:33:50 -0700

    t

--- web.py
+++ web.py
@@ -12,7 +12,7 @@ def hello():
     out =  subprocess.Popen(['gpio', 'readall'],  stdout=subprocess.PIPE, 
            stderr=subprocess.STDOUT)
     stdout,stderr = out.communicate()
-    body +="<br/><h2>GPIO status</h2>
+    body +="<br/><h2>GPIO status</h2>"
     body += stdout.decode("utf-8") 
     return body
 
