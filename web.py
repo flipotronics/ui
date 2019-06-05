16c4f8fa43e3e3f0a2a86966b3d8816c0e18a9b2
mathias-dietrich <mathias.dietrich@gmail.com>
2019-06-04 17:22:55 -0700

    add

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import os
+import subprocess
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ def hello():
     body = "<h1 style='color:blue'>Flipotronics environment variables</h1>"
     for k, v in os.environ.items(): 
     	body += k + "=" + v + "<br/>"
-
+    body +=  subprocess.run(['gpio', 'readll'], stdout=subprocess.PIPE)
     return body
 
 if __name__ == "__main__":
